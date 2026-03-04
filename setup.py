ba9fb35a7952f569d539d6748762f90c636dce06
TreeSalt <asanchez1@gmail.com>
2026-03-04 18:22:28 -0500

    chore: self-healing synchronization of project state

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="antigravity_harness",
-    version="4.7.48",
+    version="4.7.49",
     packages=find_packages(),
     install_requires=[
         "numpy",
