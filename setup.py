a4bc1bb4322569b9c6f3d67c89599675ea2c2538
TreeSalt <asanchez1@gmail.com>
2026-03-05 04:02:38 -0500

    Fiduciary Engine Online: Router upgraded, Protocol restored, Recovery repaired

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="antigravity_harness",
-    version="4.7.53",
+    version="4.7.54",
     packages=find_packages(),
     install_requires=[
         "numpy",
