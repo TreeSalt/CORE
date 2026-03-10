eb4aee379ec882880c55299f3232566f538f3e20
TreeSalt <asanchez1@gmail.com>
2026-03-10 11:46:27 -0400

    chore: final v9.9.15 artifacts and state synchronization

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="antigravity_harness",
-    version="9.9.14",
+    version="9.9.16",
     packages=find_packages(),
     install_requires=[
         "numpy",
