80a4743a0ca0dac4648f26ad1b2f80d2f680551e
TreeSalt <asanchez1@gmail.com>
2026-03-10 11:33:51 -0400

    chore: sync v9.9.12 manifest, state, and recent prompts

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="antigravity_harness",
-    version="9.9.11",
+    version="9.9.12",
     packages=find_packages(),
     install_requires=[
         "numpy",
