6e0854dfffcb8935a970cdba4d9f0bc7ddc1155f
TreeSalt <asanchez1@gmail.com>
2026-03-12 12:24:47 -0400

    restore: Zoo E1 ratification + quadrupled workload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="antigravity_harness",
-    version="9.9.42",
+    version="9.9.43",
     packages=find_packages(),
     install_requires=[
         "numpy",
