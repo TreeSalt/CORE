b77ee6a913faaf3aedea2e696fc712a9e379b040
TreeSalt <asanchez1@gmail.com>
2026-03-11 21:56:05 -0400

    fix: manual pandas addition to STDLIB

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="antigravity_harness",
-    version="9.9.38",
+    version="9.9.39",
     packages=find_packages(),
     install_requires=[
         "numpy",
