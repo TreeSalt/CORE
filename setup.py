47bd64e2d9883f9725dcf9b347f4d8a7ae92dfd3
TreeSalt <asanchez1@gmail.com>
2026-03-22 18:26:58 -0400

    fix: version 10.0.0 in __init__.py, setup.py; gitignore forge build_tmp

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="mantis_core",
-    version="9.9.107",
+    version="10.0.0",
     packages=find_packages(),
     install_requires=[
         "numpy",
